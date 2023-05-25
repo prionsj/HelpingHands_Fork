08ff519d7d009245d1ab1239e45b9d3209c35b71
Elena Väth <elena.vaeth@breuninger.de>
2023-05-25 13:39:55 +0200

    add user- and helping-service and integrate in helping-app

---
+++ helpingservice/src/components/Help.jsx
@@ -0,0 +1,8 @@
+export const Help = () => {
+    return (
+        <div>
+            <h1>Helping Service</h1>
+
+        </div>
+    );
+}
---
+++ userservice/src/components/User.jsx
@@ -0,0 +1,8 @@
+export const User = () => {
+    return (
+        <div>
+            <h1>User Service</h1>
+
+        </div>
+    );
+}
