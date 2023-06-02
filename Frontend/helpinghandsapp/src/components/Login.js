944a3b36fdcd17226bfe3fc7ef7814f56760d85f
ayse.22 <aysekocak.2202@gmail.com>
2023-06-02 10:22:12 +0200

    login and registration form

--- Frontend/helpinghandsapp/src/components/Login.js
+++ Frontend/helpinghandsapp/src/components/Login.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
+import logo from "./static/HelpingHands.png"
 import {NavLink} from "react-router-dom";
 
-const Login = () => {
+
+export const Login = (props) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
     return (
-        <div>
-            <h1>Login</h1>
-            <p>Hier kann sich der User später einloggen.</p>
-            <button><NavLink to="/hilfsanzeigen">Einloggen</NavLink></button>
-            <button><NavLink to="/registrierung">Zur Registrierung</NavLink></button>
+        <div className="login-page">
+             <div className="logo-picture">
+                    <img className="logo" src={logo}/>
+             </div>
+             <p className="logo-description">
+             <h2>Anmelden</h2>
+            </p>
+
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label htmlFor="email">E-Mail</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="deine.email@gmail.com" id="email" name="email" />
+                <label htmlFor="password">Passwort</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+                
+                <button><NavLink to="/hilfsanzeigen">Anmelden</NavLink></button>
+            </form>
+            <button className="link-btn"><NavLink to="/Registrierung">Noch kein Konto?<br></br>Jetzt registrieren</NavLink></button>
         </div>
     )
 }
-
-export default Login;
+    export default Login;
