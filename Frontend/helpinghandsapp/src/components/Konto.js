7e6c292cf6a488a7bb299d233082a2e40f329cd2
ayse.22 <aysekocak.2202@gmail.com>
2023-06-03 14:06:08 +0200

    first try mein konto page

--- Frontend/helpinghandsapp/src/components/Konto.js
+++ Frontend/helpinghandsapp/src/components/Konto.js
@@ -3,11 +3,31 @@ import Navigation from "./Navigation";
 
 const Konto = () => {
     return (
-        <div>
-            <Navigation />
-            <h1>Mein Konto</h1>
-            <p>Hier stehen später die persönlichen Daten des Benutzers.</p>
+        <div className="container"> 
+        <div className="header">
+          <h1>Mein Konto</h1>
         </div>
+        <div className="box"> 
+          <p><strong>Vorname:</strong> Ayse </p>
+          <p><strong>Nachname:</strong> Kocak</p>
+        </div>
+        <div className="box"> 
+          <p><strong>Straße:</strong> Rappenweg</p>
+          <p><strong>Hausnummer:</strong> 6</p>
+        </div>
+        <div className="box"> 
+          <p><strong>Postleitzahl:</strong> 743873</p>
+          <p><strong>Stadt:</strong> Villingen</p>
+        </div>
+        <div className="box"> 
+          <p><strong>Email:</strong> asxjsaco</p>
+          <p><strong>Telefon:</strong> 283720</p>
+        </div>
+        <div className="box"> 
+          <p><strong>Nutzername:</strong> dckjdj</p>
+          <p><strong>Passwort:</strong> ********</p>
+        </div>
+      </div>
     )
 }
 
