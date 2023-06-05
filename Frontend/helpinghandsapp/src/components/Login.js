5fcc9b5affb7092bf9d579be0e6f3fcf4c3a0134
angelikakleimann <angelika.kleimann01@gmail.com>
2023-06-05 14:57:11 +0200

    Login-page: Eigabe mit Datensatz vergleichen und Pop-Up mit inkorrekten Daten anzeigen

--- Frontend/helpinghandsapp/src/components/Login.js
+++ Frontend/helpinghandsapp/src/components/Login.js
@@ -1,77 +1,104 @@
 import React, { useState, useContext, useEffect } from "react";
 import logo from "./static/HelpingHands.png";
-import { NavLink } from "react-router-dom";
+import { useNavigate, NavLink } from "react-router-dom";
 import UsernameContext from "./UsernameContext";
 import Modal from 'react-modal';
 
 
 export const Login = ({ setUsername }) => {
-    const [nutzername, setNutzername] = useState('');
-    const [isOpen, setIsOpen] = useState(false);
+    const navigate = useNavigate();
+    const [nutzername, setNutzername] = useState("");
+    const [passwort, setPasswort] = useState("");
+    const [showPopup, setShowPopup] = useState(false);
+    const [benutzer, setBenutzer] = useState([]);
 
     const handleLogin = (e) => {
-        e.preventDefault();
-        setUsername(nutzername);
-        setIsOpen(true);
-    }
-    const [benutzer, setBenutzer] = useState([])
-
+      e.preventDefault();
+      setUsername(nutzername);
+      const matchingBenutzer = benutzer.find(
+        (benutzer) =>
+          benutzer.nutzername === nutzername && benutzer.passwort === passwort
+      );
+      if (matchingBenutzer) {
+        setShowPopup(false);
+        navigate("/hilfsanzeigen");
+      } else {
+        setShowPopup(true);
+      }
+    };
+  
     useEffect(() => {
-        fetch('http://localhost:3000/benutzer')
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                setBenutzer(data);
-
-            }).catch((err) => {
-            console.log(err.message);
+      fetch("http://localhost:3000/benutzer")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setBenutzer(data);
+        })
+        .catch((err) => {
+          console.log(err.message);
         });
     }, []);
-
+  
     return (
-
-        <div className="login-page">
-            
-            <div className="logo-picture">
-                <img className="logo" src={logo} alt="Logo" />
-            </div>
-            <p className="logo-description">
-                <h2>Anmelden</h2>
-            </p>
-
-            <form className="login-form">
-                <label htmlFor="nutzername">Nutzername</label>
-                <input
-                    value={nutzername}
-                    onChange={(e) => setNutzername(e.target.value)}
-                    type="text"
-                    placeholder="Benutzername"
-                    id="nutzername"
-                    name="nutzername"
-                />
-                <label htmlFor="passwort">Passwort</label>
-                <input
-                    type="password"
-                    placeholder="Passwort"
-                    id="passwort"
-                    name="passwort"
-                />
-
-                <button onClick={handleLogin}>
-                    <NavLink to="/hilfsanzeigen">Anmelden</NavLink>
-                </button>
-            </form>
-            <Modal isOpen={isOpen} onRequestClose={() => setIsOpen(false)}>
-                <h2>Du bist angemeldet</h2>
-                {/* ... Weitere Inhalte des Pop-ups hier ... */}
-            </Modal>
-            <button className="link-btn">
-                <NavLink to="/Registrierung">
-                    Noch kein Konto?<br></br>Jetzt registrieren
-                </NavLink>
-            </button>
+      <div className="login-page">
+        <div className="logo-picture">
+          <img className="logo" src={logo} alt="Logo" />
         </div>
+        <p className="logo-description">
+          <h2>Anmelden</h2>
+        </p>
+  
+        <form className="login-form">
+          <label htmlFor="nutzername">Nutzername</label>
+          <input
+            value={nutzername}
+            onChange={(e) => setNutzername(e.target.value)}
+            type="text"
+            placeholder="Benutzername"
+            id="nutzername"
+            name="nutzername"
+          />
+          <label htmlFor="passwort">Passwort</label>
+          <input
+            value={passwort}
+            onChange={(e) => setPasswort(e.target.value)}
+            type="password"
+            placeholder="Passwort"
+            id="passwort"
+            name="passwort"
+          />
+  
+          <button onClick={handleLogin}>
+            Anmelden
+          </button>
+        </form>
+        {showPopup && (
+          <Modal
+            isOpen={true}
+            onRequestClose={() => setShowPopup(false)}
+            style={{
+              content: {
+                width: "300px",
+                height: "200px",
+                margin: "auto",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              },
+            }}
+          >
+            <h2>Benutzername oder Passwort ist inkorrekt</h2>
+          </Modal>
+        )}
+        <button className="link-btn">
+          <NavLink to="/Registrierung">
+            Noch kein Konto?
+            <br></br>Jetzt registrieren
+          </NavLink>
+        </button>
+      </div>
     );
-};
+  };
 
 export default Login;
