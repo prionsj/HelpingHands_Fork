9929db2aaa367890c4a857fd3c3562807e86c363
ayse.22 <aysekocak.2202@gmail.com>
2023-06-05 11:06:38 +0200

    Überarbeitung der mein Konto-Übersicht

--- Frontend/helpinghandsapp/src/components/Konto.js
+++ Frontend/helpinghandsapp/src/components/Konto.js
@@ -2,6 +2,7 @@ import React, {useContext, useEffect, useState} from 'react'
 import UsernameContext from "./UsernameContext";
 import Navigation from "./Navigation";
 
+
 const Konto = () => {
     const [benutzer, setBenutzer] = useState([])
 
@@ -21,39 +22,53 @@ const Konto = () => {
 
     return (
         <div>
-            <Navigation />
-        {
-            benutzer && benutzer.map((benutzer, index)=> {
-                if (benutzer.nutzername === username) {
-                    return (
-                        <div className="container">
-                            <div className="header">
-                                <h1>Mein Konto</h1>
-                            </div>
-                            <div className="box">
-                                <p><strong>Vorname:</strong> {benutzer.vorname} </p>
-                                <p><strong>Nachname:</strong> {benutzer.nachname}</p>
-                            </div>
-                            <div className="box">
-                                <p><strong>Straße:</strong> {benutzer.straße}</p>
-                                <p><strong>Hausnummer:</strong> {benutzer.hausnummer}</p>
-                            </div>
-                            <div className="box">
-                                <p><strong>Postleitzahl:</strong> {benutzer.postleitzahl}</p>
-                                <p><strong>Stadt:</strong> {benutzer.stadt}</p>
-                            </div>
-                            <div className="box">
-                                <p><strong>Email:</strong>{benutzer.email}</p>
-                                <p><strong>Telefon:</strong> {benutzer.telefon}</p>
-                            </div>
-                            <div className="box">
-                                <p><strong>Nutzername:</strong>{benutzer.nutzername}</p>
-                            </div>
-                        </div>
-                    )
-                }
+          <Navigation />
+          {
+            benutzer && benutzer.map((benutzer, index) => {
+              if (benutzer.nutzername === username) {
+                return (
+                  <div className="container">
+                    <div className="header">
+                      <h1>Mein Konto</h1>
+                    </div>
+                    <div className="box-container">
+                      <div className="box Vorname">
+                        <strong>Vorname:</strong> {benutzer.vorname}
+                      </div>
+                      <div className="box Nachname">
+                        <strong>Nachname:</strong> {benutzer.nachname}
+                      </div>
+                    </div>
+                    <div className="box-container">
+                      <div className="box Straße">
+                        <strong>Straße:</strong> {benutzer.straße}
+                      </div>
+                      <div className="box Hausnummer">
+                        <strong>Hausnummer:</strong> {benutzer.hausnummer}
+                      </div>
+                    </div>
+                    <div className="box-container">
+                      <div className="box Postleitzahl">
+                        <strong>Postleitzahl:</strong> {benutzer.postleitzahl}
+                      </div>
+                      <div className="box Stadt">
+                        <strong>Stadt:</strong> {benutzer.stadt}
+                      </div>
+                    </div>
+                    <div className="box Email">
+                      <strong>Email:</strong> {benutzer.email}
+                    </div>
+                    <div className="box Telefon">
+                      <strong>Telefon:</strong> {benutzer.telefon}
+                    </div>
+                    <div className="box Nutzername">
+                      <strong>Nutzername:</strong> {benutzer.nutzername}
+                    </div>
+                  </div>
+                )
+              }
             })
-        }
+          }
         </div>
 
     )
