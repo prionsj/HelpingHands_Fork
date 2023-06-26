eeb13dc81a68fb1527a07a6a9c0e1dfa607f04f2
Elena Väth <elena.vaeth@breuninger.de>
2023-06-26 17:52:01 +0200

    include App.css and bootstrap in ui-test so the view is displayed with the right styling

--- Frontend/helpinghandsapp/src/components/Hilfsanzeigen.cy.js
+++ Frontend/helpinghandsapp/src/components/Hilfsanzeigen.cy.js
@@ -2,9 +2,14 @@ import React from 'react';
 import { mount } from '@cypress/react18';
 import { MemoryRouter } from 'react-router-dom';
 import Hilfsanzeigen from './Hilfsanzeigen';
+import "../App.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 describe('testing filter function of <Hilfsanzeigen />', () => {
+
   beforeEach(() => {
+    cy.viewport(375, 790)
     mount(
       <MemoryRouter>
         <Hilfsanzeigen />
@@ -13,11 +18,13 @@ describe('testing filter function of <Hilfsanzeigen />', () => {
   });
 
   it('renders the Hilfsanzeigen component', () => {
+    cy.viewport(375, 790)
     // Überprüfe, ob die Komponente erfolgreich gerendert wurde
     cy.get('.hilfsanzeigen-page').should('exist');
   });
 
   it('displays help offers based on selected location', () => {
+    cy.viewport(375, 790)
     // Wähle einen Standort aus der Standortauswahl
     cy.get('.search-container input[type="search"]').type('Karlsruhe{enter}');
 
@@ -28,6 +35,7 @@ describe('testing filter function of <Hilfsanzeigen />', () => {
   });
 
   it('displays help offers based on selected category', () => {
+    cy.viewport(375, 790)
     // Wähle eine Kategorie aus der Kategorienauswahl
     cy.get('.category-container select').select('Garten');
 
@@ -38,6 +46,7 @@ describe('testing filter function of <Hilfsanzeigen />', () => {
   });
 
   it('displays help offers based on selected category and location', () => {
+    cy.viewport(375, 790)
     // Wähle eine Kategorie aus der Kategorienauswahl
     cy.get('.category-container select').select('Garten');
 
